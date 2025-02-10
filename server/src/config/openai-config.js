65bb119be616d7458177ad4336a30680e060152c
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2025-02-11 01:37:26 +0700

    [feat]:update backend

---
+++ server/src/config/openai-config.js
@@ -0,0 +1,10 @@
+const { Configuration } = require("openai");
+
+const configureOpenAI = () => {
+  const config = new Configuration({
+    apiKey: process.env.OPEN_AI_SECRET,
+    organization: process.env.OPENAI_ORAGANIZATION_ID,
+  });
+  return config;
+};
+module.exports = { configureOpenAI };
